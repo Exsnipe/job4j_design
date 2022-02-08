548c16b3c000f69dddb4c5a85c9a3240b13a77f4
Exsnipe <exsnipe@yandex.ru>
2022-02-07 20:02:41 -0800

    BufferedReader. [#252489 #261360]

--- src/main/java/ru/job4j/io/LogFilter.java
+++ src/main/java/ru/job4j/io/LogFilter.java
@@ -7,20 +7,18 @@ import java.util.stream.Collectors;
 
 public class LogFilter {
     public List<String> filter(String file) {
-        List<String> list = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            list = reader
+            return reader
                     .lines()
+                    .filter(s -> {
+                        String[] str = s.split(" ");
+                        return "404".equals(str[str.length - 2]);
+                    })
                     .collect(Collectors.toList());
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        return list.stream()
-                .filter(s -> {
-                    String[] str = s.split(" ");
-                    return str[str.length - 2].equals("404");
-                })
-                .collect(Collectors.toList());
+        return new ArrayList<>();
     }
 
     public void save(List<String> log, String file) {
