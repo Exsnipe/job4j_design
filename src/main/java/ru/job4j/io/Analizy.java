d348e38a1760e3bc93f61b1ef8849113d24e52e1
Exsnipe <exsnipe@yandex.ru>
2022-02-10 21:39:06 -0800

    Анализ доступности сервера. [#859 #262692]

--- src/main/java/ru/job4j/io/Analizy.java
+++ src/main/java/ru/job4j/io/Analizy.java
@@ -6,38 +6,41 @@ import java.io.FileReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
 
 public class Analizy {
     public void unavailable(String source, String target) {
         List<String> list = new ArrayList<>();
-        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
-           list = reader.lines()
-                    .collect(Collectors.toList());
+        try (BufferedReader reader = new BufferedReader(new FileReader(source));
+        PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
+           AtomicBoolean started = new AtomicBoolean(false);
+            reader.lines()
+                   .filter(
+                           line -> {
+                               String[] divided = line.split(" ", 2);
+                               if (!started.get() && ("500".equals(divided[0]) || "400".equals(divided[0]))) {
+                                   started.set(true);
+                                   return true;
+                               }
+                               if (started.get() && ("200".equals(divided[0]) || "300".equals(divided[0]))) {
+                                   started.set(false);
+                                   return true;
+                               }
+                               return  false;
+                           }
+                   )
+                    .forEach(
+                            line -> {
+                                if (started.get()) {
+                                    out.print(line.split(" ", 2)[1] + ";");
+                                } else {
+                                    out.println(line.split(" ", 2)[1] + ";");
+                                }
+                            }
+                    );
         } catch (Exception ex) {
             ex.printStackTrace();
         }
-        try (PrintWriter out = new PrintWriter(new FileOutputStream(target))) {
-            String start = "";
-            boolean started = false;
-            for (String code : list) {
-                String[] divided = code.split(" ", 2);
-                if (("500".equals(divided[0]) || "400".equals(divided[0])) && !started) {
-                    started = true;
-                    start = divided[1];
-                }
-                if (("200".equals(divided[0]) || "300".equals(divided[0])) && started) {
-                    started = false;
-                    out.println(start + ";" + divided[1] + ";");
-                }
-            }
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    public static void main(String[] args) {
-        Analizy analizy = new Analizy();
-        analizy.unavailable("source.txt", "target.txt");
     }
 }
