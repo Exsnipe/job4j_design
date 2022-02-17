67870bb65e79485939a0c7bf64ae449b315ca7ea
Exsnipe <exsnipe@yandex.ru>
2022-02-16 20:52:03 -0800

    Сканирование файловой системы. [#106929]

---
+++ src/main/java/ru/job4j/io/SearchFiles.java
@@ -0,0 +1,47 @@
+package ru.job4j.io;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.FileVisitor;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+public class SearchFiles implements FileVisitor<Path> {
+    final private Predicate<Path> condition;
+    private  List<Path> listPath = new ArrayList<>();
+
+    public List<Path> getListPath() {
+        return listPath;
+    }
+
+    public SearchFiles(Predicate<Path> condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        if (condition.test(file)) {
+            listPath.add(file);
+        }
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        return FileVisitResult.CONTINUE;
+    }
+}
