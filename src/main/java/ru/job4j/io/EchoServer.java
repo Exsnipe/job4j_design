a2bd7579ad9eac93fa5a36eaacc3913ea13004f8
Exsnipe <exsnipe@yandex.ru>
2022-03-26 23:17:34 -0700

    Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,6 +6,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,14 +20,24 @@ public class EchoServer {
                 try (OutputStream out = socket.getOutputStream()) {
                     BufferedReader in = new BufferedReader(
                             new InputStreamReader(socket.getInputStream()));
-                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
                     firstLine = in.readLine();
+                    String[] answer = firstLine.split(" ");
+                    String msgAnswer = "";
+                    if ("/?msg=Bye".equals(answer[1])) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        out.write("Завершить работу сервера".getBytes());
+                        server.close();
+                        continue;
+                    }
+                    if ("/?msg=Hello".equals(answer[1])) {
+                        msgAnswer = "Hello";
+                    } else {
+                        msgAnswer = "What";
+                    }
+                    out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                    out.write(msgAnswer.getBytes());
                     System.out.println(firstLine);
                     out.flush();
-                }
-                String[] answer = firstLine.split(" ");
-                if ("/?msg=Bye".equals(answer[1])) {
-                    server.close();
                 }
             }
         }
