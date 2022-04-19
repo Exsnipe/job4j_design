bf859a7bfaa34d68217d69e64f2d665d46d558f8
Exsnipe <exsnipe@yandex.ru>
2022-04-19 13:20:52 -0700

    Связи между таблицами [#373615]

---
+++ src/main/java/ru/job4j/tables.sql
@@ -0,0 +1,42 @@
+create table person(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table car(
+    id serial primary key,
+    owner_id int references person(id)
+);
+
+create table wife(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table husbend(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table husbends_wifes(
+    id serial primary key,
+    husbends_id int references husbend(id) unique,
+    wifes_id int references wife(id) unique
+);
+
+create table employees(
+    id serial primary key,
+    name varchar(255)
+);
+
+create table projects(
+    id serial primary key,
+    project_name varchar(255)
+);
+
+create  table projects_employees(
+    id serial primary  key,
+    employees_id int references employee(id),
+    employees_name varchar(255) references employee(name),
+    projects_name varchar(255) references project(name)
+);
