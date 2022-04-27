6358291461df325920bb8e7f31d038c9e03606ee
Exsnipe <exsnipe@yandex.ru>
2022-04-27 10:11:44 -0700

    Схема прав пользователи и роли. [#1093]

--- src/main/java/ru/job4j/insert.sql
+++ src/main/java/ru/job4j/insert.sql
@@ -1,10 +1,10 @@
-insert into users(name, role_id) values(Ivanov, 1);
-insert into roles(role_name) values("admin");
-insert into us_rules(rule) values("full acces");
+insert into roles(role_name) values('admin');
+insert into users(name, role_id) values('Ivanov', 1);
+insert into us_rules(rule) values('full acces');
 insert into roles_us_rules(role_id, rule_id) values(1, 1);
-insert into items(item_name, user_id, comment_id, attach_id, category_id, state_id)
-    values ("milk", 1, 1, 1, 1, 1);
-insert into comments(item_comment) values("dom 11.11.2021");
-insert into attachs(url_file) values("C:\comments\comment1.txt");
-insert into category(category_name) values("food");
-insert into state(status) values("ordered");
+insert into category(category_name) values('food');
+insert into state(status) values('ordered');
+insert into items(item_name, user_id, category_id, state_id)
+    values ('milk', 1, 1, 1);
+insert into comments(item_comment) values('dom 11.11.2021');
+insert into attachs(url_file) values('C:\comments\comment1.txt');
