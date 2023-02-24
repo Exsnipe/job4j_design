5d7cc12d17eceb532f7db051b27e8265b1b1eae0
Exsnipe <exsnipe@yandex.ru>
2023-02-24 11:15:10 +0300

    String - это массив [#504884]

--- src/main/java/ru/job4j/JavaNameValidator.java
+++ src/main/java/ru/job4j/JavaNameValidator.java
@@ -3,16 +3,12 @@ package ru.job4j;
 public class JavaNameValidator {
     public static boolean isNameValid(String name) {
         if (name.isEmpty() || !Character.isLowerCase(name.charAt(0))) {
-            System.out.println("first if");
             return false;
         }
         for (int index = 1; index < name.length(); index++) {
-            System.out.println(index);
             int code = name.codePointAt(index);
-            System.out.println(code);
             if (!(isSpecialSymbol(code) || isUpperLatinLetter(code)
                 || isLowerLatinLetter(code) || Character.isDigit(name.charAt(index)))) {
-                System.out.println("because of " + index);
                 return false;
             }
         }
@@ -29,9 +25,5 @@ public class JavaNameValidator {
 
     public static boolean isLowerLatinLetter(int code) {
         return code >= 97 && code <= 122;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(isNameValid("fIRST"));
     }
 }
