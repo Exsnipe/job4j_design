f807d9e92042fe6bab8e004bb0ff718faa2c9c62
Exsnipe <exsnipe@yandex.ru>
2023-03-08 21:37:26 +0300

    Интерфейс Queue [#504942]

--- src/main/java/ru/job4j/collection/queue/AppleStore.java
+++ src/main/java/ru/job4j/collection/queue/AppleStore.java
@@ -3,23 +3,23 @@ package ru.job4j.collection.queue;
 import java.util.Queue;
 
 public class AppleStore {
-    private final Queue<Customer> queue;
+    private final Queue<Customer> customerQueue;
     private final int amount;
 
     public AppleStore(Queue<Customer> queue, int amount) {
-        this.queue = queue;
+        this.customerQueue = queue;
         this.amount = amount;
     }
 
     public String getLastHappyCustomer() {
         for (int index = 0; index < amount - 1; index++) {
-            queue.poll();
+            customerQueue.poll();
         }
-        return queue.poll().name();
+        return customerQueue.poll().name();
     }
 
     public String getFirstUpsetCustomer() {
         this.getLastHappyCustomer();
-        return queue.poll().name();
+        return customerQueue.poll().name();
     }
 }
