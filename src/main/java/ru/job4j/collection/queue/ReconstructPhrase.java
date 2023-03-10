af8b62f114373f9dc237281b50b222afdec26629
Exsnipe <exsnipe@yandex.ru>
2023-03-10 07:58:45 +0300

    Интерфейс Deque [#504943]

---
+++ src/main/java/ru/job4j/collection/queue/ReconstructPhrase.java
@@ -0,0 +1,40 @@
+package ru.job4j.collection.queue;
+
+import java.util.Deque;
+import java.util.Iterator;
+
+public class ReconstructPhrase {
+    private final Deque<Character> descendingElements;
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder result = new StringBuilder();
+        int elementSize = evenElements.size();
+        for (int index = 0; index < elementSize; index++) {
+            if (index % 2 == 0) {
+                result.append(evenElements.poll());
+            } else {
+                evenElements.poll();
+            }
+        }
+        return result.toString();
+    }
+
+    private String getDescendingElements() {
+       StringBuilder result = new StringBuilder();
+       Iterator<Character> descendingIterator = descendingElements.descendingIterator();
+       while (descendingIterator.hasNext()) {
+           result.append(descendingIterator.next());
+       }
+       return result.toString();
+    }
+
+    public String getReconstructPhrase() {
+        return getEvenElements() + getDescendingElements();
+    }
+}
