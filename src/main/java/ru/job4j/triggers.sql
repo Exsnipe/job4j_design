473b530e95feb0bb1eba6f7843adf75438ebfe30
Exsnipe <exsnipe@yandex.ru>
2023-04-26 08:51:49 +0300

    2. Триггеры [#504803]

---
+++ src/main/java/ru/job4j/triggers.sql
@@ -0,0 +1,59 @@
+create or replace function tax()
+	returns trigger as
+$$
+	begin
+		update products
+		set price = price * 1.13
+		where id = (select id from inserted);
+		return new;
+	end;
+$$
+LANGUAGE 'plpgsql';
+
+create or replace trigger tax
+after insert on products
+referencing new table as inserted
+for each statement
+execute procedure tax();
+
+create or replace function tax()
+	returns trigger as
+$$
+	begin
+		update products
+		set price = price * 1.13
+		where id = new.id;
+		return new;
+	end;
+$$
+language 'plpgsql';
+
+create or replace trigger tax
+after insert on products
+for each row
+execute procedure tax();
+
+create table history_of_price (
+	id serial primary key,
+	name varchar(50),
+	price integer,
+	date timestamp
+);
+
+create or replace function input_history_price()
+	returns trigger as
+$$
+	begin
+		insert into history_of_price(name, price, date)
+		values(new.name, new.price, CURRENT_DATE);
+		return new;
+	end;
+$$
+language 'plpgsql';
+
+create or replace trigger input_history_price
+after insert on products
+for each row
+execute procedure input_history_price();
+
+
