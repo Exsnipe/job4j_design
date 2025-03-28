21659df1bc76b5c0210a9a0e8656457c226a66c2
exsnipe <exsnipe@yandex.ru>
2025-03-28 13:41:48 +0300

    Жадный алгоритм [#505160]

---
+++ src/test/java/ru/job4j/algo/GasStation.java
@@ -0,0 +1,25 @@
+package ru.job4j.algo;
+
+public class GasStation {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int currentIndex;
+        int gasRemaining;
+        for (int i = 0; i < gas.length; i++) {
+            currentIndex = i;
+            gasRemaining = gas[currentIndex];
+            while (gasRemaining >= cost[currentIndex]) {
+                gasRemaining -= cost[currentIndex];
+                if (currentIndex == gas.length - 1) {
+                    currentIndex = 0;
+                } else {
+                    currentIndex++;
+                }
+                gasRemaining += gas[currentIndex];
+                if (i == currentIndex) {
+                    return i;
+                }
+            }
+        }
+        return  - 1;
+    }
+}
