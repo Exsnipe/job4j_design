ac68ef866d575186dd525b093d29898963ae174a
exsnipe <exsnipe@yandex.ru>
2025-04-18 11:52:42 +0300

    Бинарное дерево поиска. Очистка дерева. [#504988]

--- src/main/java/ru/job4j/newcoll/tree/binarytree/BinarySearchTree.java
+++ src/main/java/ru/job4j/newcoll/tree/binarytree/BinarySearchTree.java
@@ -55,6 +55,35 @@ public class BinarySearchTree<T extends Comparable<T>> {
         }
     }
 
+    public void clear() {
+        Node node = root;
+        clear(node);
+    }
+
+    private Node clear(Node node) {
+        if (node == null) {
+            return null;
+        }
+        node.left = clear(node.left);
+        node.right = clear(node.right);
+        node.key = null;
+        return null;
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        int[] array = new int[]{2, 1, 10, 6, 14, 4, 8, 12, 16, 11, 9, 13, 15, 17, 3, 5, 7};
+        for (int i : array) {
+            bst.put(i);
+        }
+        System.out.println(bst);
+        System.out.println(bst.remove(10));
+        System.out.println("После удаления узла 10 :");
+        System.out.println(bst);
+        bst.clear();
+        System.out.println(bst.inSymmetricalOrder().size());
+    }
+
     public boolean remove(T key) {
         boolean result = false;
         if (Objects.nonNull(key) && Objects.nonNull(root)) {
@@ -97,18 +126,6 @@ public class BinarySearchTree<T extends Comparable<T>> {
             current = null;
         }
         return result;
-    }
-
-    public static void main(String[] args) {
-        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
-        int[] array = new int[]{2, 1, 10, 6, 14, 4, 8, 12, 16, 11, 9, 13, 15, 17, 3, 5, 7};
-        for (int i : array) {
-            bst.put(i);
-        }
-        System.out.println(bst);
-        System.out.println(bst.remove(10));
-        System.out.println("После удаления узла 10 :");
-        System.out.println(bst);
     }
 
     private void swap(boolean isLeft, Node source, Node parent, Node current, Node equal) {
--- src/test/java/ru/job4j/newcoll/tree/binarytree/BinarySearchTreeTest.java
+++ src/test/java/ru/job4j/newcoll/tree/binarytree/BinarySearchTreeTest.java
@@ -109,4 +109,14 @@ public class BinarySearchTreeTest {
         assertThat(tree.inSymmetricalOrder()).containsExactly(1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12,
                 13, 14, 15, 16, 17);
     }
+
+    @Test
+    public void whenTreeCleared() {
+        BinarySearchTree<Integer> tree = new BinarySearchTree();
+        for (int element : new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}) {
+            tree.put(element);
+        }
+        tree.clear();
+        assertThat(tree.inSymmetricalOrder().size()).isEqualTo(1);
+    }
 }
