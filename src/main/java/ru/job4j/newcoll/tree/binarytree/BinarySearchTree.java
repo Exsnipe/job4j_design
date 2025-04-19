0de7f40d22035f713d9693c1b30122fdfb0ff59d
exsnipe <exsnipe@yandex.ru>
2025-04-19 14:40:23 +0300

    Реализация балансировки узлов AVL-дерева [#504993]

--- src/main/java/ru/job4j/newcoll/tree/binarytree/BinarySearchTree.java
+++ src/main/java/ru/job4j/newcoll/tree/binarytree/BinarySearchTree.java
@@ -56,8 +56,7 @@ public class BinarySearchTree<T extends Comparable<T>> {
     }
 
     public void clear() {
-        Node node = root;
-        clear(node);
+        root = clear(root);
     }
 
     private Node clear(Node node) {
@@ -72,6 +71,8 @@ public class BinarySearchTree<T extends Comparable<T>> {
 
     public static void main(String[] args) {
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        System.out.println(bst.inSymmetricalOrder().size());
+        System.out.println(bst);
         int[] array = new int[]{2, 1, 10, 6, 14, 4, 8, 12, 16, 11, 9, 13, 15, 17, 3, 5, 7};
         for (int i : array) {
             bst.put(i);
@@ -80,8 +81,6 @@ public class BinarySearchTree<T extends Comparable<T>> {
         System.out.println(bst.remove(10));
         System.out.println("После удаления узла 10 :");
         System.out.println(bst);
-        bst.clear();
-        System.out.println(bst.inSymmetricalOrder().size());
     }
 
     public boolean remove(T key) {
